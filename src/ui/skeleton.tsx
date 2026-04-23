afa3be09d51d3c6076ee293d34d34a16206f37a1
fidhal-ht <fidhal@hephtal.co.uk>
2026-04-23 12:16:56 +0100

    fix: skeleton height bug (#31)

--- src/ui/skeleton.tsx
+++ src/ui/skeleton.tsx
@@ -7,7 +7,7 @@ interface SkeletonProps {
 
 const Skeleton = ({ className }: SkeletonProps) => (
   <div aria-live="polite" aria-busy="true" className={className}>
-    <span className="inline-flex w-full animate-pulse select-none rounded-md bg-muted-foreground leading-none">
+    <span className="inline-flex w-full h-full animate-pulse select-none rounded-md bg-muted-foreground leading-none">
       ‌
     </span>
     <br />
